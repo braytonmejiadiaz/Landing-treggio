33b833310503ac2f62094abf4824ade2956417f0
braytonmejiadiaz <brayton.mejia13@gmail.com>
2025-03-27 19:10:22 -0500

    f

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {},
-
+	theme: {
+		extend: {
+		  animation: {
+			fadeIn: 'fadeIn 0.5s ease-in-out',
+			fadeOut: 'fadeOut 0.5s ease-in-out',
+		  },
+		  keyframes: {
+			fadeIn: {
+			  '0%': { opacity: '0' },
+			  '100%': { opacity: '1' },
+			},
+			fadeOut: {
+			  '0%': { opacity: '1' },
+			  '100%': { opacity: '0' },
+			},
+		  },
+		},
+	  },
 
 	plugins: [],
 }
